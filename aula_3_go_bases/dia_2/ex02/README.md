7699c3a0fbba01fcf06dcc43e1e8887adbd58b51
anolivei <anolivei@student.42sp.org.br>
2022-05-24 22:20:59 -0300

    a3 d2 ex2

---
+++ aula_3_go_bases/dia_2/ex02/README.md
@@ -0,0 +1,16 @@
+# Exercício 2 - E-commerce (Parte II)
+
+Uma grande empresa de vendas na web precisa adicionar funcionalidades para
+adicionar produtos aos usuários.
+Para fazer isso, eles exigem que usuários e produtos tenham o
+mesmo endereço de memória no main do programa e nas funções.
+
+Estruturas necessárias:
+- Usuário: Nome, Sobrenome, E-mail, Produtos (array de produtos).
+- Produto: Nome, preço, quantidade.
+
+Algumas funções necessárias:
+- Novo produto: recebe nome e preço, e retorna um produto.
+- Adicionar produto: recebe usuário, produto e quantidade, não retorna nada,
+adiciona o produto ao usuário.
+- Deletar produtos: recebe um usuário, apaga os produtos do usuário.
---
+++ aula_3_go_bases/dia_2/ex02/main.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type usuario struct {
+	nome string
+	sobrenome string
+	email string
+	produtos []produto
+}
+
+type produto struct {
+	nome string
+	preco float64
+	quantidade int
+}
+
+func novoProduto(nome string, preco float64) produto {
+	p := produto{nome, preco, 0}
+	return p
+}
+
+func adicionarProduto(u *usuario, p *produto, quantidade int) {
+	p.quantidade = quantidade
+	u.produtos = append(u.produtos, *p)
+}
+
+func deletarProduto(u *usuario) {
+	u.produtos = make([]produto, 0)
+}
+
+func main() {
+	u := usuario{"Joao", "das Neves", "jdasneves@gmail.com", nil}
+	p1 := novoProduto("notebook", 5000)
+	adicionarProduto(&u, &p1, 100)
+	p2 := novoProduto("geladeira", 3000)
+	adicionarProduto(&u, &p2, 100)
+	fmt.Println(u.produtos)
+	deletarProduto(&u)
+	fmt.Println(u.produtos)
+}
