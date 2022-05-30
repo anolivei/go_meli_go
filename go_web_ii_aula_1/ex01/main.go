87d34c2a60acc6ce2f14436735b05f649b6c7f9f
anolivei <anolivei@student.42sp.org.br>
2022-05-30 20:54:09 -0300

    web ii aula1 ex01

--- go_web_ii_aula_1/ex01/main.go
+++ go_web_ii_aula_1/ex01/main.go
@@ -1,5 +1,52 @@
 package main
 
-func main() {
+import (
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+)
+
+type user struct {
+	Id int `json:"id"`
+	Nome string `json:"nome"`
+	Sobrenome string `json:"sobrenome"`
+	Email string `json:"email"`
+	Idade int `json:"idade"`
+	Altura float32 `json:"altura"`
+	Ativo bool `json:"ativo"`
+	DataCriacao string `json:"datacriacao"`
+}
+
+func listUsers(users *[]user) (gin.HandlerFunc) {
+	fn := func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": &users})
+	}
+	return fn
+}
+
+func saveUsers(users *[]user, id int) (gin.HandlerFunc) {
+	fn := func(c *gin.Context) {
+		var u user
+		if err := c.ShouldBindJSON(&u); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H {"error": err.Error(),})
+			return
+		}
+		id++
+		u.Id = id
+		*users = append(*users, u)
+		c.JSON(http.StatusOK, gin.H{"data": u,})
+	}
+	return fn
+}
+
+func main() {
+	var users []user
+	var id int = 0
+	router := gin.Default()
+	group := router.Group("users")
+	{
+		group.GET("/", listUsers(&users))
+		group.POST("/", saveUsers(&users, id))
+	}
+	router.Run()
 }
