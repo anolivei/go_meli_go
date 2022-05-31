aab143e0ce9adccbb5875243c96f1b6b011d6cf8
anolivei <anolivei@student.42sp.org.br>
2022-05-30 23:35:52 -0300

    go web ii a1 ex02

--- go_web_ii_aula_1/ex02/README.md
+++ go_web_ii_aula_1/ex02/README.md
@@ -6,6 +6,6 @@ para isso devem ser seguidos os seguintes passos:
 1. Todos os campos enviados na solicitação devem ser validados, todos os
 campos são obrigatórios
 
-2. Caso algum campo não esteja completo, um código de erro 400 deve ser retornado
-com a mensagem “campo %s é obrigatório”.
+2. Caso algum campo não esteja completo, um código de erro 400 deve ser
+retornado com a mensagem “campo %s é obrigatório”.
 (Em %s deve ir o nome do campo que não está completo).
--- go_web_ii_aula_1/ex02/main.go
+++ go_web_ii_aula_1/ex02/main.go
@@ -1,5 +1,71 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"net/http"
 
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+)
+
+type user struct {
+	Id int `json:"id"`
+	Nome string `json:"nome" validate:"required"`
+	Sobrenome string `json:"sobrenome" validate:"required"`
+	Email string `json:"email" validate:"required,email"`
+	Idade int `json:"idade" validate:"required,gte=0,lt=150"`
+	Altura float32 `json:"altura" validate:"required"`
+	Ativo bool `json:"ativo" validate:"required"`
+	DataCriacao string `json:"datacriacao" validate:"required"`
+}
+
+func listUsers(users *[]user) (gin.HandlerFunc) {
+	fn := func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{"data": &users})
+	}
+	return fn
+}
+
+func saveUsers(users *[]user, id int) (gin.HandlerFunc) {
+	fn := func(c *gin.Context) {
+		var validate *validator.Validate = validator.New()
+		var u user
+		if err := c.ShouldBindJSON(&u); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H {"error": err.Error(),})
+			return
+		}
+		err := validate.Struct(u)
+		if err != nil {
+			if _, ok := err.(*validator.InvalidValidationError); ok {
+				fmt.Println(err)
+				return
+			}
+			for _, err := range err.(validator.ValidationErrors) {
+				if err != nil {
+					s := fmt.Sprintf("campo %s é obrigatório", err.Field())
+					log.Println(s)
+					c.JSON(http.StatusBadRequest, gin.H{"error": s,})
+					return
+				}
+			}
+		}
+		id++
+		u.Id = id
+		*users = append(*users, u)
+		c.JSON(http.StatusOK, gin.H{"data": u,})
+	}
+	return fn
+}
+
+func main() {
+	var users []user
+	var id int = 0
+	router := gin.Default()
+	group := router.Group("users")
+	{
+		group.GET("/", listUsers(&users))
+		group.POST("/", saveUsers(&users, id))
+	}
+	router.Run()
 }
