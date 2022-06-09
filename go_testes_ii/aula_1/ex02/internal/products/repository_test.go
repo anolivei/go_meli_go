bd7ccd25bfd8a9cd4f0ea00c659922244d4c733f
anolivei <anolivei@student.42sp.org.br>
2022-06-08 21:35:54 -0300

    go tests ii a1 ex02

--- go_testes_ii/aula_1/ex02/internal/products/repository_test.go
+++ go_testes_ii/aula_1/ex02/internal/products/repository_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func TestGetAll(t *testing.T) {
-	myStubProduct := store.New(store.FileType, "./test.json")
+	myStubProduct := store.New(store.FileType, "./test_get_all.json")
 	product := NewRepository(myStubProduct)
 
 	var expectedProduct []Product
@@ -22,7 +22,7 @@ func TestGetAll(t *testing.T) {
 }
 
 func TestUpdateName(t *testing.T) {
-	beforeUpdate := store.New(store.FileType, "./test.json")
+	beforeUpdate := store.New(store.FileType, "./test_update_name.json")
 	productBefore := NewRepository(beforeUpdate)
 	afterUpdate, _ := productBefore.UpdateName(1, "torradeira")
 
@@ -30,6 +30,19 @@ func TestUpdateName(t *testing.T) {
 	expectedProduct = append(
 					expectedProduct,
 					Product{1, "torradeira", "eletrodomestico", 200, 525.25})
+	assert.Equal(t, expectedProduct[0], afterUpdate)
+	assert.True(t, beforeUpdate.ReadWasCalled())
+}
+
+func TestUpdateNameNotFound(t *testing.T) {
+	beforeUpdate := store.New(store.FileType, "./test_update_name.json")
+	productBefore := NewRepository(beforeUpdate)
+	afterUpdate, _ := productBefore.UpdateName(2, "torradeira")
+
+	var expectedProduct []Product
+	expectedProduct = append(
+					expectedProduct,
+					Product{ID:0, Name:"", Typee:"", Count:0, Price:0})
 	assert.Equal(t, expectedProduct[0], afterUpdate)
 	assert.True(t, beforeUpdate.ReadWasCalled())
 }
