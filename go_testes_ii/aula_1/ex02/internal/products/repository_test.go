37780d2b708508bfd64c43c66ad8a5cb2604e30f
anolivei <anolivei@student.42sp.org.br>
2022-06-08 22:00:59 -0300

    go tests ii a1 ex02

--- go_testes_ii/aula_1/ex02/internal/products/repository_test.go
+++ go_testes_ii/aula_1/ex02/internal/products/repository_test.go
@@ -8,13 +8,13 @@ import (
 )
 
 func TestGetAll(t *testing.T) {
-	myStubProduct := store.New(store.FileType, "./test_get_all.json")
-	product := NewRepository(myStubProduct)
-
 	var expectedProduct []Product
-	expectedProduct = append(
-					expectedProduct,
+	expectedProduct = append(expectedProduct,
 					Product{1, "microondas", "eletrodomestico", 200, 525.25})
+	myStubProduct := store.New(store.FileType, "./test.json")
+	myStubProduct.Write(expectedProduct)
+
+	product := NewRepository(myStubProduct)
 
 	result, _ := product.GetAll()
 
@@ -22,27 +22,40 @@ func TestGetAll(t *testing.T) {
 }
 
 func TestUpdateName(t *testing.T) {
-	beforeUpdate := store.New(store.FileType, "./test_update_name.json")
-	productBefore := NewRepository(beforeUpdate)
-	afterUpdate, _ := productBefore.UpdateName(1, "torradeira")
+	var prod []Product
+	prod = append(prod,
+				Product{1, "microondas", "eletrodomestico", 200, 525.25})
+	beforeUpdate := store.New(store.FileType, "./test.json")
+	beforeUpdate.Write(prod)
 
-	var expectedProduct []Product
-	expectedProduct = append(
-					expectedProduct,
+	productBefore := NewRepository(beforeUpdate)
+	afterUpdate, err := productBefore.UpdateName(1, "torradeira")
+
+	var productAfter []Product
+	productAfter = append(
+					productAfter,
 					Product{1, "torradeira", "eletrodomestico", 200, 525.25})
-	assert.Equal(t, expectedProduct[0], afterUpdate)
+	assert.Equal(t, productAfter[0], afterUpdate)
 	assert.True(t, beforeUpdate.ReadWasCalled())
+	assert.Nil(t, err)
 }
 
 func TestUpdateNameNotFound(t *testing.T) {
-	beforeUpdate := store.New(store.FileType, "./test_update_name.json")
-	productBefore := NewRepository(beforeUpdate)
-	afterUpdate, _ := productBefore.UpdateName(2, "torradeira")
+	var prod []Product
+	prod = append(prod,
+				Product{1, "microondas", "eletrodomestico", 200, 525.25})
+	beforeUpdate := store.New(store.FileType, "./test.json")
+	beforeUpdate.Write(prod)
 
-	var expectedProduct []Product
-	expectedProduct = append(
-					expectedProduct,
+	productBefore := NewRepository(beforeUpdate)
+	afterUpdate, err := productBefore.UpdateName(2, "torradeira")
+
+	var productAfter []Product
+	productAfter = append(
+					productAfter,
 					Product{ID:0, Name:"", Typee:"", Count:0, Price:0})
-	assert.Equal(t, expectedProduct[0], afterUpdate)
+
+	assert.Equal(t, productAfter[0], afterUpdate)
 	assert.True(t, beforeUpdate.ReadWasCalled())
+	assert.NotNil(t, err)
 }
