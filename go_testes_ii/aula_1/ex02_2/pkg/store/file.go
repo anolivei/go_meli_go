8af894e333195fa2991e5a6a39e8d90bea343213
anolivei <anolivei@student.42sp.org.br>
2022-06-10 14:05:15 -0300

    mocking

---
+++ go_testes_ii/aula_1/ex02_2/pkg/store/file.go
@@ -0,0 +1,74 @@
+package store
+
+import (
+	"encoding/json"
+	"os"
+)
+
+type Store interface {
+	Read(data interface{}) error
+	Write(data interface{}) error
+	AddMock(mock *Mock)
+	ClearMock()
+}
+
+type Type string
+
+const (
+	FileType Type = "file"
+)
+
+func New(store Type, fileName string) Store {
+	switch store {
+	case FileType:
+		return &FileStore{FileName: fileName,}
+	}
+	return nil
+}
+
+type Mock struct {
+	Data []byte
+	Err  error
+}
+
+type FileStore struct {
+	FileName string
+	Mock *Mock
+}
+
+func (fs *FileStore) AddMock(mock *Mock) {
+	fs.Mock = mock
+}
+
+func (fs *FileStore) ClearMock() {
+	fs.Mock = nil
+}
+
+func (fs *FileStore) Read(data interface{}) error {
+	if fs.Mock != nil {
+		if fs.Mock.Err != nil {
+			return fs.Mock.Err
+		}
+		return json.Unmarshal(fs.Mock.Data, data)
+	}
+	file, err := os.ReadFile(fs.FileName)
+	if err != nil {
+		return err
+	}
+	return json.Unmarshal(file, &data)
+}
+
+func (fs *FileStore) Write(data interface{}) error {
+	if fs.Mock != nil {
+		if fs.Mock.Err != nil {
+			return fs.Mock.Err
+		}
+		return json.Unmarshal(fs.Mock.Data, data)
+	}
+	fileData, err := json.MarshalIndent(data, "", "  ")
+	if err != nil {
+		return err
+	}
+	return os.WriteFile(fs.FileName, fileData, 0644)
+}
+
---
+++ go_testes_ii/aula_1/ex02_2/pkg/web/response.go
@@ -0,0 +1,16 @@
+package web
+
+import "strconv"
+
+type Response struct {
+	Code string `json:"code"`
+	Data interface{} `json:"data,omitempty"`
+	Error string `json:"error,omitempty"`
+}
+
+func NewResponse(code int, data interface{}, err string) Response {
+	if code < 300 {
+		return Response{strconv.Itoa(code), data, ""}
+	}
+	return Response{strconv.Itoa(code), nil, err}
+}
